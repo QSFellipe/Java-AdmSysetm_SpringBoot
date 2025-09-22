a9db4d12fc5679d050af579d98dabe6a49657592
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2025-09-22 15:34:38 -0300

    Classes de serviços com os métodos criados

--- src/main/java/com/admSystem/financialSystem/service/EntradasSaidasService.java
+++ src/main/java/com/admSystem/financialSystem/service/EntradasSaidasService.java
@@ -1,14 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.admSystem.financialSystem.service;
 
-/**
- *
- * @author mathe
- */
-public class EntradasSaidasService {
+import com.admSystem.financialSystem.data.EntradasSaidasEntity;
+import com.admSystem.financialSystem.data.EntradasSaidasRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class EntradasSaidasService {
+    @Autowired
+    EntradasSaidasRepository entradasSaidasRepo;
+    
+    /*metodo para criar as entradas/saidas*/
+    public EntradasSaidasEntity createEntradasSaidas(EntradasSaidasEntity eSaidas){
+        eSaidas.setId(null);
+        entradasSaidasRepo.save(eSaidas);
+        return eSaidas;
+    }
+    
+    /*metodo para criar as entradas/saidas*/
+    public EntradasSaidasEntity getEntradasSaidasId(Long eSaidasId){
+        return entradasSaidasRepo.findById(eSaidasId).orElse(null);
+    }
+    
+    /*metodo para listar todas as entradas/saidas*/
+    public List<EntradasSaidasEntity> listEntradasSaidas(){
+        return entradasSaidasRepo.findAll();
+    }
+    
+    /*metodo para atualizar as entradas/saidas*/
+    public EntradasSaidasEntity atualizarES (Long eSaidasId, EntradasSaidasEntity eSRequest){
+        EntradasSaidasEntity eSEntity = getEntradasSaidasId(eSaidasId);
+        
+        eSEntity.setCategoria(eSRequest.getCategoria());
+        eSEntity.setDataEntrada(eSRequest.getDataEntrada());
+        eSEntity.setDataSaida(eSRequest.getDataSaida());
+        eSEntity.setDescricao(eSRequest.getDescricao());
+        eSEntity.setMetodoPagamento(eSRequest.getMetodoPagamento());
+        eSEntity.setValorEntrada(eSRequest.getValorEntrada());
+        eSEntity.setValorSaida(eSRequest.getValorSaida());
+        
+        entradasSaidasRepo.save(eSEntity);
+        
+        return eSEntity;
+        
+    }
+    /*metodo para deletar as entradas/saidas*/
+    public void deleteEntradasSaidasId(Long eSaidasId){
+        EntradasSaidasEntity eSEntity = getEntradasSaidasId(eSaidasId);
+        
+        entradasSaidasRepo.deleteById(eSEntity.getId());
+    }
 }
--- src/main/java/com/admSystem/financialSystem/service/UserService.java
+++ src/main/java/com/admSystem/financialSystem/service/UserService.java
@@ -1,14 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.admSystem.financialSystem.service;
 
-/**
- *
- * @author mathe
- */
-public class UserService {
+import com.admSystem.financialSystem.data.UserEntity;
+import com.admSystem.financialSystem.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class UserService {
+    @Autowired
+    UserRepository userRepo;
+    
+     /*metodo para criar o usuário*/
+    public UserEntity createUser(UserEntity user){
+        user.setId(null);
+        userRepo.save(user);
+        return user;
+    }
+    
+    /*metodo get para identificação do user dentro do sistema*/
+    public UserEntity getUserId(Long userId){
+        return userRepo.findById(userId).orElse(null);
+    }
 }
