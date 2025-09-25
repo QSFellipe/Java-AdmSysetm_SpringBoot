f3a4b7236c81cee850e2edf76bc6f61b1e7c92c0
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2025-09-25 20:31:49 -0300

    Controller User

--- src/main/java/com/admSystem/financialSystem/controller/UserController.java
+++ src/main/java/com/admSystem/financialSystem/controller/UserController.java
@@ -1,7 +1,36 @@
-
-
 package com.admSystem.financialSystem.controller;
 
-public class UserController {
+import com.admSystem.financialSystem.data.UserEntity;
+import com.admSystem.financialSystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "*")
+public class UserController {
+    @Autowired
+    UserService userService;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> getUserId(@PathVariable Long id){
+        UserEntity userId = userService.getUserId(id);
+        
+        return new ResponseEntity<>(userId, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user){
+        var newUsew = userService.createUser(user);
+        
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+        
 }
