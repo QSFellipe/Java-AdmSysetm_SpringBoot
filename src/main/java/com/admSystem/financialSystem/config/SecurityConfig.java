c073346dba5872eaee1ca548c19a4eca1dcda3d0
Fellipe Queiroz Silva <qsfellipe@gmail.com>
2026-03-13 20:29:08 -0300

    Security Config
    Inclusão de configurações que possibilitam a utilização das telas e recursos que não são nativos do Security Web

--- src/main/java/com/admSystem/financialSystem/config/SecurityConfig.java
+++ src/main/java/com/admSystem/financialSystem/config/SecurityConfig.java
@@ -2,12 +2,36 @@ package com.admSystem.financialSystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+         
+                .authorizeHttpRequests(auth ->{ 
+                    auth
+                        .requestMatchers("/login", "/register", "/css/**", "/js/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated();
+                })
+                
+                .formLogin(httpForm ->{
+                    httpForm
+                        .loginPage("/login").permitAll();
+                })             
+                
+                .build();
+    }
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
